66df8790ce3ebd7494198eaa375347bff6eeda35
Bernardo <beefreguglia@gmail.com>
2024-09-25 18:56:57 -0300

    fetaure: using cookies to config a session and create controller

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,52 +1,28 @@
 import { FastifyInstance } from 'fastify'
-import { z } from 'zod'
-import { randomUUID } from 'node:crypto'
-import { knex } from '@/database/knex'
+
+import { checkSessionIdExists } from '@/middlewares/check-session-id-exists'
+import { TransactionController } from '@/controllers/transactionsController'
+
+const transactionController = new TransactionController()
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    transactionController.index,
+  )
 
-    return { transactions }
-  })
+  app.get(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    transactionController.show,
+  )
 
-  app.get('/:id', async (request) => {
-    const getTransactionParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    transactionController.summary,
+  )
 
-    const { id } = getTransactionParamsSchema.parse(request.params)
-
-    const transaction = await knex('transactions').where({ id }).first()
-
-    return { transaction }
-  })
-
-  app.post('/', async (request, reply) => {
-    const createTransactionBodySchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
-    })
-
-    const { title, amount, type } = createTransactionBodySchema.parse(
-      request.body,
-    )
-
-    await knex('transactions').insert({
-      id: randomUUID(),
-      title,
-      amount: type === 'credit' ? amount : amount * -1,
-    })
-
-    return reply.status(201).send()
-  })
-
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
-
-    return { summary }
-  })
+  app.post('/', transactionController.create)
 }
