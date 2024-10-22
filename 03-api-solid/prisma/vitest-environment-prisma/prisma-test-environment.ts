2a89e9f9c545ce99c4429fb03eff4d49dfae8874
Bernardo <beefreguglia@gmail.com>
2024-10-22 13:30:23 -0300

    feature: create authenticate e2e test

--- 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -7,12 +7,14 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-function generateDatabaseUrl(schema: string) {
+function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
     throw new Error('Please provide a DATABASE_URL environment variable.')
   }
 
   const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
   return url.toString()
 }
 
@@ -20,15 +22,18 @@ export default <Environment>{
   name: 'prisma',
   async setup() {
     const schema = randomUUID()
-    process.env.DATABASE_URL = generateDatabaseUrl(schema)
+    const databaseURL = generateDatabaseURL(schema)
 
-    execSync('pnpm dlx prisma migrate deploy')
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
         await prisma.$executeRawUnsafe(
           `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
         )
+
         await prisma.$disconnect()
       },
     }
---
+++ 03-api-solid/src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,30 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    const name = 'John Doe'
+    const email = 'johndoe@example.com'
+    const password = '123456'
+
+    await request(app.server).post('/register').send({ name, email, password })
+
+    const response = await request(app.server)
+      .post('/sessions')
+      .send({ email, password })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
