d039ba161b97e212a1b5956f749e92255fe458e4
Bernardo <beefreguglia@gmail.com>
2024-10-22 16:06:21 -0300

    feature: create metrics and validate check-in e2e tests

---
+++ 03-api-solid/src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,65 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check-in metrics (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list the total count of check-ins', async () => {
+    const name = 'John Doe'
+    const email = 'johndoe@example.com'
+    const password = '123456'
+
+    const { token } = await createAndAuthenticateUser(app, {
+      email,
+      name,
+      password,
+    })
+
+    const title = 'Javascript Gym'
+    const description = 'Some description'
+    const phone = '11999999999'
+    const latitude = -20.3992387
+    const longitude = -43.5105982
+
+    const user = await prisma.user.findFirstOrThrow()
+    const gym = await prisma.gym.create({
+      data: {
+        latitude,
+        longitude,
+        title,
+        description,
+        phone,
+      },
+    })
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkInsCount).toEqual(2)
+  })
+})
