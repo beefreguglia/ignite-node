f239a9a3bb9a1125c8850aaa61d2034b00edb14e
Bernardo <beefreguglia@gmail.com>
2024-10-23 17:54:08 -0300

    feature: create models and config prisma

---
+++ 03-find-a-friend-api/prisma/migrations/20241023204927_create_organization_pets_address_and_requiriments_models/migration.sql
@@ -0,0 +1,80 @@
+-- CreateEnum
+CREATE TYPE "EnergyLevel" AS ENUM ('VERY_LOW', 'LOW', 'MEDIUM', 'HIGH', 'VERY_HIGH');
+
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('SMALL', 'MEDIUM', 'LARGE');
+
+-- CreateEnum
+CREATE TYPE "Space" AS ENUM ('SMALL', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "Depends" AS ENUM ('SMALL', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "addresses" (
+    "id" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" INTEGER,
+    "neighborhood" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "complement" TEXT,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "owner_name" TEXT NOT NULL,
+    "phone" TEXT,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "address_id" TEXT NOT NULL,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+    "size" "Size" NOT NULL,
+    "energy_level" "EnergyLevel" NOT NULL,
+    "space" "Space" NOT NULL,
+    "depends" "Depends" NOT NULL,
+    "organization_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "requirements" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "pet_id" TEXT,
+
+    CONSTRAINT "requirements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_email_key" ON "organizations"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_address_id_key" ON "organizations"("address_id");
+
+-- AddForeignKey
+ALTER TABLE "organizations" ADD CONSTRAINT "organizations_address_id_fkey" FOREIGN KEY ("address_id") REFERENCES "addresses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "requirements" ADD CONSTRAINT "requirements_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE SET NULL ON UPDATE CASCADE;
