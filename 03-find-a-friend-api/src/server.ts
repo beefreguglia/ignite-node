7b22359868cf06ba115a5570aa033ddaf50e94df
Bernardo <beefreguglia@gmail.com>
2024-10-23 12:25:07 -0300

    feature: connect env in project

--- 03-find-a-friend-api/src/server.ts
+++ 03-find-a-friend-api/src/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log(`🚀 HTTP Server Running on port 3333 🚀`)
+    console.log(`🚀 HTTP Server Running on port ${env.PORT} 🚀`)
   })
