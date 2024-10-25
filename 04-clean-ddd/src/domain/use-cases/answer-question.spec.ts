98de7c19ba8910f1b2d1e54386b38b71fe016c74
Bernardo <beefreguglia@gmail.com>
2024-10-25 13:41:10 -0300

    feature: create answers repositories

--- 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
+++ 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,19 @@
 import { expect, test } from 'vitest'
 
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repositories'
+import { Answer } from '../entities/answer'
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async(answer:Answer) => {
+    return
+  }
+}
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     content: 'Nova resposta',
     instructorId: '1',
     questionId: '1'
