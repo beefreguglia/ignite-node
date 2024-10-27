fbcc491e6be1ec3122d6609713abc96f05722e34
Bernardo <beefreguglia@gmail.com>
2024-10-27 12:18:12 -0300

    feature: create comments entity

--- 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comments.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comments.ts
@@ -1,39 +1,14 @@
-import { Entity } from '../../../../core/entities/entity'
 import { UniqueEntityID } from '../../../../core/entities/unique-entity-id'
 import { Optional } from '../../../../core/types/optional'
+import { Comment, CommentProps } from './comments'
 
-export interface AnswerCommentProps {
-  authorID: UniqueEntityID
+export interface AnswerCommentProps extends CommentProps {
   answerID: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
-  get authorID() {
-    return this.props.authorID
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class AnswerComment extends Comment<AnswerCommentProps> {
+  get answerID() {
+    return this.props.answerID
   }
 
   static create(
