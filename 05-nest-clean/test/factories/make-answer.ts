f47d7627a2dd749313868de489ec67c2921d7348
Bernardo <beefreguglia@gmail.com>
2024-11-05 23:01:49 -0300

    refactor: create question comments, answer comments and answer factories

--- 05-nest-clean/test/factories/make-answer.ts
+++ 05-nest-clean/test/factories/make-answer.ts
@@ -5,6 +5,9 @@ import {
   Answer,
   AnswerProps,
 } from '../../src/domain/forum/enterprise/entities/answer'
+import { Injectable } from '@nestjs/common'
+import { PrismaAnswerMapper } from '@/infra/database/prisma/mappers/prisma-answer-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeAnswer(
   override?: Partial<AnswerProps>,
@@ -21,4 +24,19 @@ export function makeAnswer(
   )
 
   return answer
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(data: Partial<AnswerProps> = {}): Promise<Answer> {
+    const answer = makeAnswer(data)
+
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    })
+
+    return answer
+  }
 }
